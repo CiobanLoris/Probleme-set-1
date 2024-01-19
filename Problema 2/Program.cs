04cf83fb0b2f6434e06c624aeba21ea9ce9ea9ea
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:21:02 +0200

    Add files via upload

---
+++ Problema 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduceti b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduceti c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta > 0)
+        {
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine($"Solutiile ecuatiei sunt x1 = {x1} si x2 = {x2}");
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine($"Ecuatia are o solutie dubla x = {x}");
+        }
+        else
+        {
+            double partereala = -b / (2 * a);
+            double parteimaginara = Math.Sqrt(-delta) / (2 * a);
+            Console.WriteLine($"Solutiile complexe ale ecuatiei sunt: {partereala} + {parteimaginara}i si {partereala} - {parteimaginara}i");
+        }
+
+        
+        Console.ReadKey();
+    }
+}
