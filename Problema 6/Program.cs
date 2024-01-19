fe5316c083cb679075e042211477d263a420de66
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:32:23 +0200

    Add files via upload

---
+++ Problema 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea laturii a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduceti lungimea laturii b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduceti lungimea laturii c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        if (IsTriangle(a, b, c))
+        {
+            Console.WriteLine($"Cele trei lungimi {a}, {b}, {c} pot forma laturile unui triunghi.");
+        }
+        else
+        {
+            Console.WriteLine($"Cele trei lungimi {a}, {b}, {c} nu pot forma laturile unui triunghi.");
+        }
+
+        
+        Console.ReadKey();
+    }
+
+    static bool IsTriangle(double a, double b, double c)
+    {
+        return a + b > c && a + c > b && b + c > a;
+    }
+}
