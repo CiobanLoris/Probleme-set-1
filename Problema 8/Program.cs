5dc9d84c2ae10d89e5c863bec77ec6ffea00d820
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:33:08 +0200

    Add files via upload

---
+++ Problema 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti valoarea pentru a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduceti valoarea pentru b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Valori initiale: a = {a}, b = {b}");
+
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"Valori inversate: a = {a}, b = {b}");
+
+
+        Console.ReadKey();
+    }
+}
