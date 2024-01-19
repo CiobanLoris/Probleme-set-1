6662ee1cc8fd9f61d5189f8fb3a041e56a549a29
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:39:22 +0200

    Add files via upload

---
+++ Problema 19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti numarul: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (repetare(number))
+        {
+            Console.WriteLine($"{number} este format doar cu 2 cifre care se pot repeta.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} nu este format doar cu 2 cifre care se pot repeta.");
+        }
+
+        
+        Console.ReadKey();
+    }
+
+    static bool repetare(int n)
+    {
+        
+        string numberString = n.ToString();
+
+        
+        return numberString.Distinct().Count() == 2;
+    }
+}
