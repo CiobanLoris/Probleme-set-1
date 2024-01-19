8fcc5bdd10086e650cd855eb7076cad393e7376c
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:25:10 +0200

    Add files via upload

---
+++ Problema 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti anul: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if (IsLeapYear(year))
+        {
+            Console.WriteLine($"{year} este un an bisect.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} nu este un an bisect.");
+        }
+
+        
+        Console.ReadKey();
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
