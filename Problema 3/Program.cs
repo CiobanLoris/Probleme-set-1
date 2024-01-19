531cdee1a6a74ac8792a32070d468971bd6ac418
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:23:20 +0200

    Add files via upload

---
+++ Problema 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceti k: ");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        if (k != 0 && n % k == 0)
+        {
+            Console.WriteLine($"{n} se divide cu {k}.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} nu se divide cu {k}.");
+        }
+
+        
+        Console.ReadKey();
+    }
+}
