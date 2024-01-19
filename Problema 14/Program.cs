f30f5d26812a561b4009fb163c2d912742ce1143
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:37:44 +0200

    Add files via upload

---
+++ Problema 14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti numarul n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (palindrom(n))
+        {
+            Console.WriteLine($"{n} este un numar palindrom.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} nu este un numar palindrom.");
+        }
+
+        
+        Console.ReadKey();
+    }
+
+    static bool palindrom(int number)
+    {
+        int nr = number;
+        int invers = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            invers = invers * 10 + digit;
+            number /= 10;
+        }
+
+        return nr == invers;
+    }
+}
