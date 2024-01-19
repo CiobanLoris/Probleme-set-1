5fbf84a7526e87e6b3f73cdadaef9bd948538c54
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:35:13 +0200

    Add files via upload

---
+++ Problema 12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceti a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceti b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int count = div_n(n, a, b);
+
+        Console.WriteLine($"In intervalul [{a}, {b}] exista {count} numere intregi divizibile cu {n}.");
+
+        
+        Console.ReadKey();
+    }
+
+    static int div_n(int n, int a, int b)
+    {
+        int count = 0;
+
+        for (int i = a; i <= b; i++)
+        {
+            if (i % n == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
