890f4dfc2f12a3a33b0c588f15c2e08c74be8f13
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:34:45 +0200

    Add files via upload

---
+++ Problema 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti numarul n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Cifrele numarului {n} in ordine inversa sunt: {inversare(n)}");
+
+        
+        Console.ReadKey();
+    }
+
+    static int inversare(int number)
+    {
+        int nrinvers = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            nrinvers = nrinvers * 10 + digit;
+            number /= 10;
+        }
+
+        return nrinvers;
+    }
+}
